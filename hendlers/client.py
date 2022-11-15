b01b2b9aadc91cb2dba885ef752227f91e8aaefe
NurbekPy <nurbekseitaliev@icloud.com>
2022-11-15 18:17:30 +0600

    dice game

--- hendlers/client.py
+++ hendlers/client.py
@@ -1,7 +1,7 @@
 from aiogram import Dispatcher, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import dp, bot
-
+from asyncio import sleep
 
 #@dp.message_handler(commands=['quiz'])
 async def quiz_1(message: types.Message):
@@ -36,7 +36,27 @@ async def pin_1(message: types.Message):
         await bot.pin_chat_message(message.chat.id, message.message_id)
 
 
+async def start_dice(message: types.Message):
+    await bot.send_message(message.from_user.id, f'{message.from_user.full_name} , игра начинается!')
+    await sleep(1)
+    bot_data = await bot.send_dice(message.from_user.id)
+    bot_data = bot_data['dice']['value']
+    await sleep(5)
+
+    user_data = await bot.send_dice(message.from_user.id)
+    user_data = user_data['dice']['value']
+    await sleep(5)
+
+    if bot_data > user_data:
+        await bot.send_message(message.from_user.id, 'Ты проиграл!')
+    elif bot_data < user_data:
+        await bot.send_message(message.from_user.id, 'Ты победил!')
+    else:
+        await bot.send_message(message.from_user.id, 'Ничья!')
+
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_message_handler(photo_1, commands=['mem'])
     dp.register_message_handler(pin_1, commands=['!pin'])
+    dp.register_message_handler(start_dice, commands=['start'])
